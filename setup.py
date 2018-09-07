520879f0e1938b112d0ac400583b0151c3a24fae
Dmitry Pechnikov <dspechnikov@gmail.com>
2018-09-07 12:38:45 +0300

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-slugger',
-    version='1.1.0',
+    version='1.1.1',
     url='https://gitlab.com/dspechnikov/django-slugger/',
     description='Automatic slug field for Django.',
     license='MIT',
